6e7ffdc42eb668a3c4ddcfd85982eb4dda1f72dc
tartarus-git <niktedig@gmail.com>
2024-08-08 15:56:02 +0200

    Did the error handling functions. Now I just need to use them from the rest of the code.

--- error_handling.c
+++ error_handling.c
@@ -1,3 +1,33 @@
 #include "error_handling.h"
 
-// TODO
+#include <stdlib.h>
+#include <stdio.h>
+
+void print_log(log_entry_type_t entry_type, const char * const msg)
+{
+	const char *entry_type_string;
+	switch (entry_type) {
+	case LET_INFO: entry_type_string = "INFO"; break;
+	case LET_WARNING: entry_type_string = "WARNING"; break;
+	case LET_ERROR: entry_type_string = "ERROR"; break;
+	case LET_FATAL_ERROR: entry_type_string = "FATAL ERROR"; break;
+	case LET_BUG: entry_type_string = "BUG"; break;
+	case LET_FATAL_BUG: entry_type_string = "FATAL BUG"; break;
+	}
+
+	printf("tcp-mirror: %s: %s\n", entry_type_string, msg);
+}
+
+void print_log_fatal_error_and_exit(const char * const msg)
+{
+	print_log(LET_FATAL_ERROR, msg);
+	print_log(LET_FATAL_ERROR, "exited");
+	exit(EXIT_FAILURE);
+}
+
+void print_log_fatal_bug_and_exit(const char * const msg)
+{
+	print_log(LET_FATAL_BUG, msg);
+	print_log(LET_FATAL_ERROR, "exited");
+	exit(EXIT_FAILURE);
+}
--- error_handling.h
+++ error_handling.h
@@ -1,6 +1,18 @@
 #ifndef ERROR_HANDLING_H_GUARD
 #define ERROR_HANDLING_H_GUARD
 
-// TODO
+typedef enum log_entry_type_t {
+	LET_INFO,
+	LET_WARNING,
+	LET_ERROR,
+	LET_FATAL_ERROR,
+	LET_BUG,
+	LET_FATAL_BUG
+} log_entry_type_t;
+
+void print_log(log_entry_type_t entry_type, const char * const msg);
+
+void print_log_fatal_error_and_exit(const char * const msg);
+void print_log_fatal_bug_and_exit(const char * const msg);
 
 #endif
